b7715cc4e61fddac7c2f23965517971fc2f8f11f
Reena Singh <aditya.kumar@calidig.com>
2023-06-07 08:43:53 +0530

    removed extra semicolon in migration file

--- database/migrations/2023_06_06_122029_create_students_table.php
+++ database/migrations/2023_06_06_122029_create_students_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('phone');
-            $table->string('email')->unique();;
+            $table->string('email')->unique();
             $table->string('state');
             $table->string('city');
             $table->string('country');
